060f9af6d5d2bc6d7b3c17f66f62d64a77293d4e
Mojtaba Amiri <mojtaba.a.amiri@outlook.com>
2025-09-10 12:20:21 +0200

    new changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@supabase/supabase-js"],
+  webpack: (config: {
+    resolve: { fallback: Record<string, boolean | string> };
+  }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      dns: false,
+      child_process: false,
+      tls: false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
